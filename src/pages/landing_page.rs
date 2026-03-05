9fa2e6240580a4ea5caf6337bd248760f9a25c8a
meirelesgc <geu_docsta@outlook.com>
2026-03-05 10:49:54 -0300

    wip

--- src/pages/landing_page.rs
+++ src/pages/landing_page.rs
@@ -1,15 +1,27 @@
 use yew::prelude::*;
-
 #[function_component]
 pub fn LandingPage() -> Html {
     html! {
         <div>
-            <section class="yellow-card">
+            <section class="yellow-card" id="home">
                 <h1>{ "Apresentação" }</h1>
+                <nav class="card-nav">
+                    <a href="#trabalho">{ "Trabalho" }</a>
+                    <a href="#perfil">{ "Perfil" }</a>
+                    <a href="#contato">{ "Contato" }</a>
+                </nav>
             </section>
 
-            <section class="more-options">
-                <h2>{ "Opções" }</h2>
+            <section id="trabalho" class="more-options">
+                <h2>{ "Trabalho" }</h2>
+            </section>
+
+            <section id="perfil" class="more-options">
+                <h2>{ "Perfil" }</h2>
+            </section>
+
+            <section id="contato" class="more-options">
+                <h2>{ "Contato" }</h2>
             </section>
         </div>
     }
